cb92b8bf705eb84c9c7b027dcb55be3e93e14b52
Mark <frozenstranger510@gmail.com>
2025-05-17 16:43:59 -0500

    cmake: Utilize interrogate to create a standalone library with Python signatures.

---
+++ deformers/config_deformer.cxx
@@ -0,0 +1,17 @@
+#include "config_deformer.h"
+#include "dconfig.h"
+
+Configure(config_surface_deformers);
+NotifyCategoryDef(surface_deformers , "");
+
+ConfigureFn(config_surface_deformers) {
+  init_libsurfacedeformers();
+}
+
+void init_libsurfacedeformers() {
+  static bool initialized = false;
+  if (initialized) return;
+
+  initialized = true;
+  return;
+}
---
+++ deformers/config_deformer.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "pandabase.h"
+#include "notifyCategoryProxy.h"
+NotifyCategoryDecl(surface_deformers, EXPORT_CLASS, EXPORT_TEMPL);
+extern void init_libsurfacedeformers();
